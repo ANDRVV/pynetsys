33116edee3988fa8ee93659772091b508937cfd0
Andrea Vaccaro <100683661+ANDRVV@users.noreply.github.com>
2022-07-05 01:30:30 +0200

    Create constants.py

---
+++ pynetsys/constants.py
@@ -0,0 +1,11 @@
+class Protocol:
+    # TRACEROUTE
+    UDP = "_udp"
+    TCP = "_tcp"
+    ICMP = "_icmp"
+
+class Packets:
+    # PACKET: Denial Of Service
+    DEATH_PING = "_dp.dos"
+    SYN_FLOOD = "_sf.dos"
+    WIRELESS_DEAUTH = "_ws.dos"
