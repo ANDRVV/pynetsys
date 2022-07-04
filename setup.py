968093b9e4bd3a4247be5d0ea21fa9941b4c55d8
Andrea Vaccaro <100683661+ANDRVV@users.noreply.github.com>
2022-07-05 01:47:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 install_reqs = parse_requirements('requirements.txt', session=False)
 dependencies = [str(ir) for ir in install_reqs]
 
-VERSION = "1.0.0"
+VERSION = "0.0.2"
 DESCRIPTION = "pynetsys is a collection of tools and malicious packets."
 
 setup(
     name="pynetsys",
     version=VERSION,
+    url="https://github.com/ANDRVV/pynetsys",
     author="Andrea Vaccaro (ANDRVV)",
     description=DESCRIPTION,
     long_description=long_description,
@@ -23,12 +24,12 @@ setup(
     packages= ["pynetsys"],
     license= "BSD 3 License",
     keywords=["python", "socket", "threading", "server", "client", "packet", "packets", "net", "network", "wireless", "dos", "tools", "tool", "traceroute", "arp", "tracert", "arping", "nslookup"],
+    install_reqs = dependencies
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+        "Operating System :: Microsoft :: Windows"]
     )
